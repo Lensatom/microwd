210fc36a77d4ea152e6ba95669d8431b40e2db73
Lensatom <ayomidetommiwa@gmail.com>
2022-10-12 10:16:24 +0100

    Pending page UI simplified

--- src/pages/admin/Pending.js
+++ src/pages/admin/Pending.js
@@ -1,27 +1,29 @@
 import React from 'react'
+import { FaCheck, FaMarker } from 'react-icons/fa'
 
 const Pending = () => {
 
   const pendings = [
-    ['Doyin David Anifowose', '+23482173426', 'doyinsanis@gmail.com'],
-    ['Doyin David Anifowose', '+23482173426', 'doyinsanis@gmail.com']
+    ['Oreoluwa Victoria Olarewaju', '+2349039086400'],
+    ['Ayomide Samuel Atayero', '+2349136852885']
   ]
 
   return (
     <div className='mt-16 w-full px-3'>
-      <h2 className=''> Pending Accounts </h2>
+      <h2 className='font-bold'> Pending Accounts </h2>
       {pendings.map(pending => {
         return (
-          <div className='flex flex-col p-5 py-8 border-b-1'>
-            <div className='w-full rounded-md flex items-center justify-between'>
-              <div className='w-16 h-16 rounded-full bg-gray-200'></div>
-              <div className='w-3/4 flex flex-col items-end'>
+          <a href={`https://wa.me/${pending[1]}`} className='flex py-8 border-b-1 justify-between'>
+            <div className='w-2/3 rounded-md flex items-start'>
+              <div className='w-16 h-16 rounded-lg bg-gray-200'></div>
+              <div className='flex ml-2 flex-col w-2/3'>
                 <h2 className='text-lg font-semibold text-gray-500'> {pending[0]} </h2>
-                <a href={`https://wa.me/${pending[1]}`} className='text-xs underline text-gray-400'> {pending[1]} </a>
               </div>
             </div>
-            <button className='bg-gray-200 text-sm py-3 mt-5 rounded-md text-gray-500 font-bold'> Authenticate Now </button>
-          </div>
+            <button className='text-sm w-16 h-16 py-3 rounded-full border-1 font-bold flex justify-center items-center'>
+              <FaCheck className='text-lg font-semibold text-cyan-800' />
+            </button>
+          </a>
         )
       })}
     </div>
