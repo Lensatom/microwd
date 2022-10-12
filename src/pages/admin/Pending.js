3da458daedcdc911797296947afde922cabb0967
Lensatom <ayomidetommiwa@gmail.com>
2022-10-12 20:41:33 +0100

    Editing pending page UI

--- src/pages/admin/Pending.js
+++ src/pages/admin/Pending.js
@@ -10,14 +10,14 @@ const Pending = () => {
 
   return (
     <div className='mt-16 w-full px-3'>
-      <h2 className='font-bold'> Pending Accounts </h2>
+      <h2 className='font-bold text-gray-900'> Pending Accounts </h2>
       {pendings.map(pending => {
         return (
-          <a href={`https://wa.me/${pending[1]}`} className='flex py-8 border-b-1 justify-between'>
+          <a href={`https://wa.me/${pending[1]}`} className='flex py-5 border-b-1 justify-between'>
             <div className='w-2/3 rounded-md flex items-start'>
               <div className='w-16 h-16 rounded-lg bg-gray-200'></div>
               <div className='flex ml-2 flex-col w-2/3'>
-                <h2 className='text-lg font-semibold text-gray-500'> {pending[0]} </h2>
+                <h2 className='font-semibold text-gray-500'> {pending[0]} </h2>
               </div>
             </div>
             <button className='text-sm w-16 h-16 py-3 rounded-full border-1 font-bold flex justify-center items-center'>
